f21fff086eb2924999fe982f110bd12ff35a18e8
Sergey Kosolapov <83372457+erpator@users.noreply.github.com>
2023-03-29 14:49:42 +0300

    commit_prog

---
+++ HW_2_Type/Program.cs
@@ -0,0 +1,34 @@
+﻿// Home work 2
+namespace homework2
+{
+    class test1
+    {
+        static void Main(string[] args)
+        {
+            long Chislo;
+            byte BChislo;
+            string TypeChisla = "byte";
+            while (true)
+            {
+                Console.WriteLine($"Введите число");
+                //string? input = Console.ReadLine();
+                bool result = long.TryParse(Console.ReadLine(), out Chislo); // todo: не учитывает числа превышающие тип long
+                if (result == true) //проверка ввода
+                {
+                    if (Chislo >= sbyte.MinValue & Chislo <= sbyte.MaxValue) //аналогично для остальных типов
+                    {
+                        BChislo = (byte)Chislo;
+                        Console.WriteLine("Ваше число, приведенное к типу " + TypeChisla + " : " + BChislo);
+                    }
+                    else Console.WriteLine("Для приведения числа " + Chislo + " к типу " + TypeChisla + " число не может быть больше " +
+                        sbyte.MaxValue + " и меньше " + sbyte.MinValue);
+                }
+                else //в слчучае если число вовсе не число
+                    Console.WriteLine("Введенные данные не являются числом");
+                //Console.WriteLine(isValid);
+                //Console.WriteLine("");
+            }
+        }
+    }
+}
+
